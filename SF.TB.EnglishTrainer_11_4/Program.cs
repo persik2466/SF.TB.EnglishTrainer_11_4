d392be5279bbec0573ec3ae8f11535078dbee9c9
Irina Lokosova <persik2466@gmail.com>
2021-11-09 19:02:15 +0400

    Добавьте файлы проекта.

---
+++ SF.TB.EnglishTrainer_11_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SF.TB.EnglishTrainer_11_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
